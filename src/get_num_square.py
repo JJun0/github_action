f7217822cd5d153265a6ca0a48e2a67d51bb72ab
JJun0 <101166558+JJun0@users.noreply.github.com>
2023-04-04 11:34:43 +0900

    Update get_num_square.py

--- src/get_num_square.py
+++ src/get_num_square.py
@@ -7,8 +7,8 @@ num = os.environ.get("INPUT_NUM")
 if num:
   try:
     num = int(num)
-    except Exception:
-      exit('ERROR: the INPUT_NUM provided ("{}") is not an integer'.format(num))
+  except Exception:
+    exit('ERROR: the INPUT_NUM provided ("{}") is not an integer'.format(num))
  else:
   num = 1
   
